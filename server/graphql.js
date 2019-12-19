eb6356c809eac978222987e2ecd5de8b91d89106
Vasiliki Delimpasi <vasiliki.delimpasi@bbc.co.uk>
2019-12-19 09:03:33 +0000

    Setup for deploying an Apollo Server with AWS Lambda via Serverless

---
+++ server/graphql.js
@@ -0,0 +1,15 @@
+const { ApolloServer } = require('apollo-server-lambda')
+const { importSchema } = require('graphql-import')
+
+const { resolvers } = require('./resolvers')
+
+const typeDefs = importSchema('schema/schema.graphql')
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+exports.graphqlHandler = server.createHandler({
+  cors: {
+    origin: true,
+    credentials: true,
+  },
+})
