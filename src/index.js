24dd487841ef0b86ef643b73918fe792fa1753f1
Vasiliki Delimpasi <vasiliki.delimpasi@bbc.co.uk>
2020-01-05 11:04:28 +0000

    Refined the app's view with personal info and the actual app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,25 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import App from "./App"
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { ApolloProvider } from '@apollo/react-hooks'
+import App from './App'
+import client from './client'
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+const Root = () => {
+  return (
+    <div className="main">
+      <div className="information">
+        <a className="githubLink" href="https://github.com/vdelimpa/search-box-app" alt="Github Repo Source Code Page">
+          <p>⚙ View source code</p>
+        </a>
+        <p className="creator">|  🛠 Made by
+          <a href="https://www.linkedin.com/in/vdelimpasi/" alt="Vasiliki Delimpasi's LinkedIn Profile"> Vasiliki Delimpasi</a>
+        </p>
+      </div>
+        <ApolloProvider client={client}>
+          <App />
+        </ApolloProvider>
+    </div>
+  )
+}
+
+ReactDOM.render(<Root />, document.getElementById('root'))
