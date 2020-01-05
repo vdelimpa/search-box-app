bd08f135e57a70ab7db498ff2230f71a3a13b927
Vasiliki Delimpasi <vasiliki.delimpasi@bbc.co.uk>
2020-01-05 17:47:00 +0000

    Update App.js to have the main app components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import React from 'react'
+import { ApolloProvider } from '@apollo/react-hooks'
 import SearchBox from './components/SearchBox'
+import client from './client'
 import './index.css'
 
 const App = () => (
-    <div className="container">
-      <SearchBox /> 
+  <div className="main">
+    <div className="information">
+      <a className="githubLink" href="https://github.com/vdelimpa/search-box-app" alt="Github Repo Source Code Page">
+        <p>⚙ View source code</p>
+      </a>
+      <p className="creator">|  🛠 Made by
+        <a href="https://www.linkedin.com/in/vdelimpasi/" alt="Vasiliki Delimpasi's LinkedIn Profile"> Vasiliki Delimpasi</a>
+      </p>
     </div>
-  )
+      <ApolloProvider client={client}>
+        <div className="container">
+          <SearchBox /> 
+        </div>
+      </ApolloProvider>
+  </div>
+)
+
 export default App
