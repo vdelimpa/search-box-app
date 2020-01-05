b66428f8e0b072c007e546b222a7dc67b6e9e9f6
Vasiliki Delimpasi <vasiliki.delimpasi@bbc.co.uk>
2020-01-05 17:44:27 +0000

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,25 +1,8 @@
+import 'react-app-polyfill/ie9'
+import 'react-app-polyfill/ie11'
+import 'react-app-polyfill/stable'
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { ApolloProvider } from '@apollo/react-hooks'
 import App from './App'
-import client from './client'
 
-const Root = () => {
-  return (
-    <div className="main">
-      <div className="information">
-        <a className="githubLink" href="https://github.com/vdelimpa/search-box-app" alt="Github Repo Source Code Page">
-          <p>⚙ View source code</p>
-        </a>
-        <p className="creator">|  🛠 Made by
-          <a href="https://www.linkedin.com/in/vdelimpasi/" alt="Vasiliki Delimpasi's LinkedIn Profile"> Vasiliki Delimpasi</a>
-        </p>
-      </div>
-        <ApolloProvider client={client}>
-          <App />
-        </ApolloProvider>
-    </div>
-  )
-}
-
-ReactDOM.render(<Root />, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
